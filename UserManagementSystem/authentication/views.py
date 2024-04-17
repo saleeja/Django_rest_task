4bf113d2d65279d6c253a77ff6c3ed405908ec59
saleeja <saleejas2@gmail.com>
2024-04-17 18:11:44 +0530

    task on 17-04

--- UserManagementSystem/authentication/views.py
+++ UserManagementSystem/authentication/views.py
@@ -26,6 +26,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
